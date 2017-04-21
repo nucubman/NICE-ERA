b74573df7c4ba9cda7c337360a878f9aebf6056c
Luka Nucubidze <lukanucubidze@yahoo.com>
2017-04-21 18:38:16 +0400

    Update and rename .gitignore to .cpp

---
+++ .cpp
@@ -0,0 +1,124 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+private:
+	
+	
+public:
+	string Name;
+	string Lastname;
+	double Amount;
+	string GetFullName(); //{
+	//	return this->Lastname + " " + this->Name; // es esec sheileba 
+	//}
+
+	Person() {
+
+	}
+
+	Person(string name, string lastname) { //  class shi shiedzleba ramdenime Person is anu konstruktoris sheqmna ogond sxvadasxva parametrebit!
+		this->Name = name;
+		this->Lastname = lastname;
+	}
+  void SetName(string name, string lastname);
+  this->Name = name;
+  this->LastName = lastname;
+};
+ 
+string Person::GetFullName() {
+	return this->Lastname + " " + this->Name;
+}
+
+
+
+void main(){
+	Person p1("saba", "sekhniashvili");
+	Person p2("sandro", "nutsubidze");
+
+	Person p3;
+  
+  
+
+	cout << p1.GetFullName() << endl;
+	cout << p2.GetFullName() << endl;
+	
+  p3.SetName(name , lastname);
+
+	system("pause");
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Person {
+private:
+	
+	
+public:
+	string Name;
+	string Lastname;
+	double Amount;
+	string GetFullName(); //{
+	//	return this->Lastname + " " + this->Name; // es esec sheileba 
+	//}
+
+	Person() {
+
+	}
+
+	Person(string name, string lastname) { //  class shi shiedzleba ramdenime Person is anu konstruktoris sheqmna ogond sxvadasxva parametrebit!
+		this->Name = name;
+		this->Lastname = lastname;
+	}
+
+	void SetName(string name, string lastname) {
+		this->Name = name;
+		this->Lastname = lastname;
+
+	}
+	
+	
+
+};
+ 
+string Person::GetFullName() {
+	return this->Lastname + " " + this->Name;
+}
+
+
+
+void main(){
+	Person p1("saba", "sekhniashvili");
+	Person p2("sandro", "nutsubidze");
+
+	Person p3;
+	string name, lastname;
+	cin >> name >> lastname;
+	
+
+	cout << p1.GetFullName() << endl;
+	cout << p2.GetFullName() << endl;
+
+	Person *p4 = new Person;
+	p4->SetName(name, lastname);
+	cout << endl << p4->GetFullName() << endl;
+	
+	Person *persons = new Person[10]; // es sheqmnis 10 cal personas ! pointeri masivze person tipis obieqtebit!
+	persons[0].GetFullName();
+
+	system("pause");
+
+}
+
+
+
+
+
